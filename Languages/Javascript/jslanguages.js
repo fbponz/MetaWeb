fc0b0ad9a6f7811fd2318301f0617294d9fa6c49
fbponz <fbponz@gmail.com>
2024-03-24 20:35:00 +0100

    Loops in javascript

--- Languages/Javascript/jslanguages.js
+++ Languages/Javascript/jslanguages.js
@@ -10,3 +10,24 @@ console.log(petDog, "says",dogSound)
 console.log(petCat, "says",catSound)
 catSound="meow"
 console.log(petCat, "now says",catSound)
+
+console.log(2+2)
+console.log(3-2)
+console.log(2*2)
+console.log(6/2)
+console.log(3>2)
+console.log(3<2)
+console.log(3==2)
+
+for( var i=1; i<= 3; i++){
+    console.log(i)
+}
+console.log('Go')
+
+var i=1;
+while(i<=3)
+{
+    console.log(i)
+    i=i+1;
+}
+console.log('Go')
